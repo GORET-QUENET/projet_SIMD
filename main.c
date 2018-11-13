10e531578465964666fb8740a7264395107c0256
Francois <francois.goretquenet@laposte.net>
2018-11-13 18:26:22 +0000

    calcul des temps

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 //Commande SSE : https://software.intel.com/sites/landingpage/IntrinsicsGuide/
 
 #define NBFRAME 300
-#define THETA 20
+#define THETA 15
 #define N 3
 #define Vmax 200
 #define Vmin 10
@@ -38,6 +38,16 @@ vuint8 _mm_sub_abs_epi8(vuint8 a, vuint8 b)
 	return _mm_MAX_epi8(_mm_sub_epi8(a,b), _mm_sub_epi8(b,a));
 }
 
+vuint8 _mm_cmplt_epu8(vuint8 a, vuint8 b)
+{
+	return _mm_cmplt_epi8(_mm_sub_epi8(a,_mm_set1_epi8(128)), _mm_sub_epi8(b,_mm_set1_epi8(128)));
+}
+
+vuint8 _mm_cmpgt_epu8(vuint8 a, vuint8 b)
+{
+	return _mm_cmpgt_epi8(_mm_sub_epi8(a,_mm_set1_epi8(128)), _mm_sub_epi8(b,_mm_set1_epi8(128)));
+}
+
 /*--------------------------------------------------------------------------------*/
 void FD(long nrl, long nrh, long ncl, long nch, uint8 **ma, uint8 **m, uint8 **mFD)
 /*--------------------------------------------------------------------------------*/
@@ -53,36 +63,14 @@ void FD(long nrl, long nrh, long ncl, long nch, uint8 **ma, uint8 **m, uint8 **m
 	{
 		for(int j = ncl; j <= nch; j+=16)
 		{
-			vm = _mm_set_epi8(m[i][j+0 ],m[i][j+1 ],m[i][j+2 ],m[i][j+3 ],
-				          m[i][j+4 ],m[i][j+5 ],m[i][j+6 ],m[i][j+7 ],
-				          m[i][j+8 ],m[i][j+9 ],m[i][j+10],m[i][j+11],
-				          m[i][j+12],m[i][j+13],m[i][j+14],m[i][j+15]);
-			
-			vma = _mm_set_epi8(ma[i][j+0 ],ma[i][j+1 ],ma[i][j+2 ],ma[i][j+3 ],
-				           ma[i][j+4 ],ma[i][j+5 ],ma[i][j+6 ],ma[i][j+7 ],
-				           ma[i][j+8 ],ma[i][j+9 ],ma[i][j+10],ma[i][j+11],
-				           ma[i][j+12],ma[i][j+13],ma[i][j+14],ma[i][j+15]);
+			vm = _mm_loadu_si128((__m128i*)&m[i][j]);
+			vma = _mm_loadu_si128((__m128i*)&ma[i][j]);
 
-			cmpl = _mm_cmplt_epi8(_mm_sub_abs_epi8(vm,vma), vTHETA);	//if( |vm - vma| < vTHETA)
+			cmpl = _mm_cmplt_epu8(_mm_sub_abs_epi8(vm,vma), vTHETA);	//if( |vm - vma| < vTHETA)
 			vmFD = _mm_or_si128(_mm_and_si128(cmpl,vnoir),			//if( |vm - vma| < vTHETA) -> vmFD = 0
 			       _mm_andnot_si128(cmpl, vblanc));				//else -> vmFD = 255
 
-			mFD[i][j+15] = (vmFD[0] >> 0 ) & 255;
-			mFD[i][j+14] = (vmFD[0] >> 8 ) & 255;
-			mFD[i][j+13] = (vmFD[0] >> 16) & 255;
-			mFD[i][j+12] = (vmFD[0] >> 24) & 255;
-			mFD[i][j+11] = (vmFD[0] >> 32) & 255;
-			mFD[i][j+10] = (vmFD[0] >> 40) & 255;
-			mFD[i][j+9 ] = (vmFD[0] >> 48) & 255;
-			mFD[i][j+8 ] = (vmFD[0] >> 56) & 255;
-			mFD[i][j+7 ] = (vmFD[1] >> 0 ) & 255;
-			mFD[i][j+6 ] = (vmFD[1] >> 8 ) & 255;
-			mFD[i][j+5 ] = (vmFD[1] >> 16) & 255;
-			mFD[i][j+4 ] = (vmFD[1] >> 24) & 255;
-			mFD[i][j+3 ] = (vmFD[1] >> 32) & 255;
-			mFD[i][j+2 ] = (vmFD[1] >> 40) & 255;
-			mFD[i][j+1 ] = (vmFD[1] >> 48) & 255;
-			mFD[i][j+0 ] = (vmFD[1] >> 56) & 255;
+			_mm_storeu_si128((__m128i*)&mFD[i][j], vmFD);
 		}
 	}
 }
@@ -121,23 +109,13 @@ void SD(long nrl, long nrh, long ncl, long nch, uint8 **m, uint8 **mSD, uint8 **
 	{
 		for(int j = ncl; j <= nch; j+=16)
 		{
-			vm = _mm_set_epi8(m[i][j+0 ],m[i][j+1 ],m[i][j+2 ],m[i][j+3 ],
-				          m[i][j+4 ],m[i][j+5 ],m[i][j+6 ],m[i][j+7 ],
-				          m[i][j+8 ],m[i][j+9 ],m[i][j+10],m[i][j+11],
-				          m[i][j+12],m[i][j+13],m[i][j+14],m[i][j+15]);
+			vm = _mm_loadu_si128((__m128i*)&m[i][j]);
+			vMSDa = _mm_loadu_si128((__m128i*)&MSDa[i][j]);
+			vVSDa = _mm_loadu_si128((__m128i*)&VSDa[i][j]);
 
-			vMSDa = _mm_set_epi8(MSDa[i][j+0 ],MSDa[i][j+1 ],MSDa[i][j+2 ],MSDa[i][j+3 ],
-					     MSDa[i][j+4 ],MSDa[i][j+5 ],MSDa[i][j+6 ],MSDa[i][j+7 ],
-					     MSDa[i][j+8 ],MSDa[i][j+9 ],MSDa[i][j+10],MSDa[i][j+11],
-					     MSDa[i][j+12],MSDa[i][j+13],MSDa[i][j+14],MSDa[i][j+15]);
 
-			vVSDa = _mm_set_epi8(VSDa[i][j+0 ],VSDa[i][j+1 ],VSDa[i][j+2 ],VSDa[i][j+3 ],
-					     VSDa[i][j+4 ],VSDa[i][j+5 ],VSDa[i][j+6 ],VSDa[i][j+7 ],
-					     VSDa[i][j+8 ],VSDa[i][j+9 ],VSDa[i][j+10],VSDa[i][j+11],
-					     VSDa[i][j+12],VSDa[i][j+13],VSDa[i][j+14],VSDa[i][j+15]);
-
-			cmpl = _mm_cmplt_epi8(vMSDa, vm);	//if(vMSDa < vm)
-			cmpg = _mm_cmpgt_epi8(vMSDa, vm);	//if(vMSDa > vm)
+			cmpl = _mm_cmplt_epu8(vMSDa, vm);	//if(vMSDa < vm)
+			cmpg = _mm_cmpgt_epu8(vMSDa, vm);	//if(vMSDa > vm)
 
 
 			vMSD = _mm_or_si128(_mm_and_si128(cmpl,_mm_add_epi8(vMSDa,one)),				//if(vMSDa < vm ) -> vMSD = vMSDa + 1
@@ -147,8 +125,8 @@ void SD(long nrl, long nrh, long ncl, long nch, uint8 **m, uint8 **mSD, uint8 **
 			vOSD = _mm_sub_abs_epi8(vMSD, vm);
 
 			NvOSD = _mm_add_epi8(vOSD,_mm_add_epi8(vOSD,vOSD));	//NvOSD = N * vOSD = 3 * vOSD
-			cmpl = _mm_cmplt_epi8(vVSDa, NvOSD);			//if(vVSDa < vOSD * 3)
-			cmpg = _mm_cmpgt_epi8(vVSDa, NvOSD);			//if(vVSDa > vOSD * 3)
+			cmpl = _mm_cmplt_epu8(vVSDa, NvOSD);			//if(vVSDa < vOSD * 3)
+			cmpg = _mm_cmpgt_epu8(vVSDa, NvOSD);			//if(vVSDa > vOSD * 3)
 
 			vVSD = _mm_or_si128(_mm_and_si128(cmpl,_mm_add_epi8(vVSDa,one)),				//if(vVSDa < vOSD * vN) -> vVSD = vVSDa + 1
 			       _mm_andnot_si128(cmpl, _mm_or_si128(_mm_and_si128(cmpg, _mm_sub_epi8(vVSDa,one)),	//else if(vVSDa > vOSD * vN) -> vVSD = vVSDa - 1
@@ -158,62 +136,13 @@ void SD(long nrl, long nrh, long ncl, long nch, uint8 **m, uint8 **mSD, uint8 **
 			vVSD = _mm_MAX_epi8(_mm_MIN_epi8(vVSD,vVmax),vVmin);	
 
 
-			cmpl = _mm_cmplt_epi8(vOSD, vVSD);		//if(vOSD < vVSD)
+			cmpl = _mm_cmplt_epu8(vOSD, vVSD);		//if(vOSD < vVSD)
 			vmSD = _mm_or_si128(_mm_and_si128(cmpl,vnoir),	//if(vOSD < vVSD) -> vmSD = 0
 			       _mm_andnot_si128(cmpl, vblanc));		//else -> vmSD = 255
 
-
-			mSD[i][j+15] = (vmSD[0] >> 0 ) & 255;
-			mSD[i][j+14] = (vmSD[0] >> 8 ) & 255;
-			mSD[i][j+13] = (vmSD[0] >> 16) & 255;
-			mSD[i][j+12] = (vmSD[0] >> 24) & 255;
-			mSD[i][j+11] = (vmSD[0] >> 32) & 255;
-			mSD[i][j+10] = (vmSD[0] >> 40) & 255;
-			mSD[i][j+9 ] = (vmSD[0] >> 48) & 255;
-			mSD[i][j+8 ] = (vmSD[0] >> 56) & 255;
-			mSD[i][j+7 ] = (vmSD[1] >> 0 ) & 255;
-			mSD[i][j+6 ] = (vmSD[1] >> 8 ) & 255;
-			mSD[i][j+5 ] = (vmSD[1] >> 16) & 255;
-			mSD[i][j+4 ] = (vmSD[1] >> 24) & 255;
-			mSD[i][j+3 ] = (vmSD[1] >> 32) & 255;
-			mSD[i][j+2 ] = (vmSD[1] >> 40) & 255;
-			mSD[i][j+1 ] = (vmSD[1] >> 48) & 255;
-			mSD[i][j+0 ] = (vmSD[1] >> 56) & 255;
-
-
-			MSD[i][j+15] = (vMSD[0] >> 0 ) & 255;
-			MSD[i][j+14] = (vMSD[0] >> 8 ) & 255;
-			MSD[i][j+13] = (vMSD[0] >> 16) & 255;
-			MSD[i][j+12] = (vMSD[0] >> 24) & 255;
-			MSD[i][j+11] = (vMSD[0] >> 32) & 255;
-			MSD[i][j+10] = (vMSD[0] >> 40) & 255;
-			MSD[i][j+9 ] = (vMSD[0] >> 48) & 255;
-			MSD[i][j+8 ] = (vMSD[0] >> 56) & 255;
-			MSD[i][j+7 ] = (vMSD[1] >> 0 ) & 255;
-			MSD[i][j+6 ] = (vMSD[1] >> 8 ) & 255;
-			MSD[i][j+5 ] = (vMSD[1] >> 16) & 255;
-			MSD[i][j+4 ] = (vMSD[1] >> 24) & 255;
-			MSD[i][j+3 ] = (vMSD[1] >> 32) & 255;
-			MSD[i][j+2 ] = (vMSD[1] >> 40) & 255;
-			MSD[i][j+1 ] = (vMSD[1] >> 48) & 255;
-			MSD[i][j+0 ] = (vMSD[1] >> 56) & 255;
-
-			VSD[i][j+15] = (vVSD[0] >> 0 ) & 255;
-			VSD[i][j+14] = (vVSD[0] >> 8 ) & 255;
-			VSD[i][j+13] = (vVSD[0] >> 16) & 255;
-			VSD[i][j+12] = (vVSD[0] >> 24) & 255;
-			VSD[i][j+11] = (vVSD[0] >> 32) & 255;
-			VSD[i][j+10] = (vVSD[0] >> 40) & 255;
-			VSD[i][j+9 ] = (vVSD[0] >> 48) & 255;
-			VSD[i][j+8 ] = (vVSD[0] >> 56) & 255;
-			VSD[i][j+7 ] = (vVSD[1] >> 0 ) & 255;
-			VSD[i][j+6 ] = (vVSD[1] >> 8 ) & 255;
-			VSD[i][j+5 ] = (vVSD[1] >> 16) & 255;
-			VSD[i][j+4 ] = (vVSD[1] >> 24) & 255;
-			VSD[i][j+3 ] = (vVSD[1] >> 32) & 255;
-			VSD[i][j+2 ] = (vVSD[1] >> 40) & 255;
-			VSD[i][j+1 ] = (vVSD[1] >> 48) & 255;
-			VSD[i][j+0 ] = (vVSD[1] >> 56) & 255;
+			_mm_storeu_si128((__m128i*)&mSD[i][j], vmSD);
+			_mm_storeu_si128((__m128i*)&MSD[i][j], vMSD);
+			_mm_storeu_si128((__m128i*)&VSD[i][j], vVSD);
 		}
 	}
 }
@@ -255,13 +184,6 @@ void SEQ_SD(long nrl, long nrh, long ncl, long nch, uint8 **m, uint8 **mSD, uint
 /**************************************************/
 /*     		  TESTS UNITAIRES		  */
 /**************************************************/
-
-int64_t clocktime() {
-   struct timeval full_time;
-
-   gettimeofday(&full_time, NULL);
-   return (int64_t) ((full_time.tv_usec + full_time.tv_sec * 1000000) / 1000);
-}
 
 void test_visage()
 {
@@ -355,6 +277,144 @@ void Generate_ROC(uint8 **m, long nrl, long nrh, long ncl, long nch, int ROC[], 
 	}
 }
 
+void test_FD(uint8 **m, long nrl, long nrh, long ncl, long nch)
+{
+	char *filename = malloc( 100 * sizeof(char));
+	uint8 **ma; // matrice t-1
+	uint8 **mFD; // matrice après FD
+
+	ma = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+	mFD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+
+	for(int step = 0; step < NBFRAME; step++)
+	{
+		sprintf(filename,"hall/hall%06d.pgm", step);
+		CopyMatrice(nrl, nrh, ncl, nch, ma, m);
+		MLoadPGM_ui8matrix(filename, nrl, nrh, ncl, nch, m);
+
+		if(step > 0)
+		{
+			FD(nrl, nrh, ncl, nch, ma, m, mFD);
+			sprintf(filename,"FD/hall%06d.pgm", step);
+			SavePGM_ui8matrix(mFD, nrl, nrh, ncl, nch, filename);
+		}
+	}
+}
+
+void test_SD(uint8 **m, long nrl, long nrh, long ncl, long nch)
+{
+	char *filename = malloc( 100 * sizeof(char));
+	uint8 **mSD; // matrice après SD
+	uint8 **MSD; // matrice Moyenne
+	uint8 **MSDa; // matrice moyenne à t-1
+	uint8 **OSD; // matrice différence
+	uint8 **VSD; // matrice Variance V
+	uint8 **VSDa; // matrice variance V à t-1
+
+	mSD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+	MSD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+	MSDa = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+	OSD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+	VSD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+	VSDa = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+
+	for(int step = 0; step < NBFRAME; step++)
+	{
+		sprintf(filename,"hall/hall%06d.pgm", step);
+		CopyMatrice(nrl, nrh, ncl, nch, MSDa, MSD);
+		CopyMatrice(nrl, nrh, ncl, nch, VSDa, VSD);
+		MLoadPGM_ui8matrix(filename, nrl, nrh, ncl, nch, m);
+
+		if(step == 0)
+		{
+			CopyMatrice(nrl, nrh, ncl, nch, MSD, m);
+			CopyValue(nrl, nrh, ncl, nch, VSD, Vmin);
+		}	
+		else
+		{
+			SD(nrl, nrh, ncl, nch, m, mSD, MSD, MSDa, OSD, VSD, VSDa);
+			sprintf(filename,"SD/hall%06d.pgm", step);
+			SavePGM_ui8matrix(mSD, nrl, nrh, ncl, nch, filename);
+		}
+	}
+}
+
+
+void test_morpho_SD(uint8 **m, long nrl, long nrh, long ncl, long nch)
+{
+	char *filename = malloc( 100 * sizeof(char));
+	uint8 **tmp;
+	
+	tmp = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+	for(int step = 0; step < NBFRAME; step++)
+	{
+		if(step)
+		{
+			sprintf(filename,"SD/hall%06d.pgm", step);
+			MLoadPGM_ui8matrix(filename, nrl, nrh, ncl, nch, m);
+
+			Erosion3(nrl, nrh, ncl, nch, m, tmp);
+			Fermeture3(nrl, nrh, ncl, nch, m, tmp);
+			Ouverture3(nrl, nrh, ncl, nch, m, tmp);
+			Fermeture5(nrl, nrh, ncl, nch, m, tmp);
+			Ouverture5(nrl, nrh, ncl, nch, m, tmp);
+			
+			
+			sprintf(filename,"SD+morpho/hall%06d.pgm", step);
+			SavePGM_ui8matrix(m, nrl, nrh, ncl, nch, filename);
+		}
+	}
+}
+
+void test_morpho_FD(uint8 **m, long nrl, long nrh, long ncl, long nch)
+{
+	char *filename = malloc( 100 * sizeof(char));
+	uint8 **tmp;
+	
+	tmp = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
+	for(int step = 0; step < NBFRAME; step++)
+	{
+		if(step)
+		{
+			sprintf(filename,"FD/hall%06d.pgm", step);
+			MLoadPGM_ui8matrix(filename, nrl, nrh, ncl, nch, m);
+
+			Fermeture3(nrl, nrh, ncl, nch, m, tmp);
+			Ouverture3(nrl, nrh, ncl, nch, m, tmp);
+			Ouverture5(nrl, nrh, ncl, nch, m, tmp);
+			Fermeture5(nrl, nrh, ncl, nch, m, tmp);
+		
+			
+			sprintf(filename,"FD+morpho/hall%06d.pgm", step);
+			SavePGM_ui8matrix(m, nrl, nrh, ncl, nch, filename);
+		}
+	}
+}
+
+void test_ROC(uint8 **m, long nrl, long nrh, long ncl, long nch, int ROC[], char* AlgoName)
+{
+	char *filename = malloc( 100 * sizeof(char));
+	for(int step = 10; step < NBFRAME; step+=10)
+	{
+		sprintf(filename,"%s/hall%06d.pgm", AlgoName, step);
+		MLoadPGM_ui8matrix(filename, nrl, nrh, ncl, nch, m);
+		Generate_ROC(m, nrl, nrh, ncl, nch, ROC, step);
+	}
+}
+
+void temps_fichier(uint8 **m, long nrl, long nrh, long ncl, long nch)
+{
+	char *filename = malloc( 100 * sizeof(char));
+	for(int step = 0; step < NBFRAME; step++)
+	{
+		if(step)
+		{
+			sprintf(filename,"SD/hall%06d.pgm", step);
+			MLoadPGM_ui8matrix(filename, nrl, nrh, ncl, nch, m);
+			SavePGM_ui8matrix(m, nrl, nrh, ncl, nch, filename);
+		}
+	}
+}
 
 /**************************************************/
 /*             FIN DES TESTS UNITAIRES            */
@@ -365,12 +425,15 @@ int main(int argc, char **argv)
 {
 	if (argc == 1)
 	{ // EXECUTION NORMALE DU PROGRAMME
-		char *filename = malloc( 100 * sizeof(char));
-		float t;
-		clock_t t1, t2;
+		float temps_SD, temps_FD, temps_morpho_SD, temps_morpho_FD;
+		int cycle_SD, cycle_FD, cycle_morpho_SD, cycle_morpho_FD;
+		clock_t t1, t2, t3, t4, t5, tfile;
 		int ROC_FD[4] = {0}; //VP, FN, FP, VN
 		int ROC_SD[4] = {0}; //VP, FN, FP, VN
+		int ROC_FD_morpho[4] = {0}; //VP, FN, FP, VN
+		int ROC_SD_morpho[4] = {0}; //VP, FN, FP, VN
 		/*  GENERATION DE LA MATRICE M  */
+		uint8 **m;
 		long nrl; 
 		long nrh;
 		long ncl;
@@ -378,16 +441,6 @@ int main(int argc, char **argv)
 		long height, width;
 		char *buffer;
 		buffer = (char*) calloc(80, sizeof(char));
-		uint8 **m; // image courante 
-		uint8 **ma; // matrice t-1
-		uint8 **mFD; // matrice après FD
-		uint8 **mSD; // matrice après SD
-		uint8 **MSD; // matrice Moyenne
-		uint8 **MSDa; // matrice moyenne à t-1
-		uint8 **OSD; // matrice différence
-		uint8 **VSD; // matrice Variance V
-		uint8 **VSDa; // matrice variance V à t-1
-		uint8 **tmp;
 		FILE *file;
 
 		file = fopen("hall/hall000000.pgm","rb");
@@ -412,78 +465,69 @@ int main(int argc, char **argv)
 		nch = width - 1;
 
 		m = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		/* POUR CALCUL FD */
-		ma = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		mFD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		/* POUR CALCUL SD */
-		mSD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		MSD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		MSDa = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		OSD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		VSD = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		VSDa = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-		tmp = ui8matrix(nrl - 2, nrh + 2, ncl - 2, nch + 2);
-
-		/*printf("nrl : %ld\n",*nrl);
-		printf("nrh : %ld\n",*nrh);
-		printf("ncl : %ld\n",*ncl);
-		printf("nch : %ld\n",*nch);*/
+		
 		/* ########################## */
 		t1 = clock();
-		for(int step = 0; step < NBFRAME; step++)
-		{
-			sprintf(filename,"hall/hall%06d.pgm", step);
-			//printf("name : %s\n", filename);
-		        CopyMatrice(nrl, nrh, ncl, nch, ma, m);
-			CopyMatrice(nrl, nrh, ncl, nch, MSDa, MSD);
-			CopyMatrice(nrl, nrh, ncl, nch, VSDa, VSD);
-			MLoadPGM_ui8matrix(filename, nrl, nrh, ncl, nch, m);
-			if(step == 0)
-			{
-				CopyMatrice(nrl, nrh, ncl, nch, MSD, m);
-				CopyValue(nrl, nrh, ncl, nch, VSD, Vmin);
-			}	
-			else
-			{
-				FD(nrl, nrh, ncl, nch, ma, m, mFD);
-				sprintf(filename,"FD/hall%06d.pgm", step);
-				SavePGM_ui8matrix(mFD, nrl, nrh, ncl, nch, filename);
-
-				/*Fermeture3(*nrl, *nrh, *ncl, *nch, mFD, tmp);
-				Fermeture5(*nrl, *nrh, *ncl, *nch, mFD, tmp);
-				sprintf(filename,"FD+morpho/hall%06d.pgm", i);
-				SavePGM_ui8matrix(mFD, *nrl, *nrh, *ncl, *nch, filename);*/
-
-				SD(nrl, nrh, ncl, nch, m, mSD, MSD, MSDa, OSD, VSD, VSDa);
-				sprintf(filename,"SD/hall%06d.pgm", step);
-				SavePGM_ui8matrix(mSD, nrl, nrh, ncl, nch, filename);
-					
-				/*Ouverture3(nrl, nrh, ncl, nch, mSD, tmp);
-				Fermeture3(nrl, nrh, ncl, nch, mSD, tmp);
-				Ouverture5(nrl, nrh, ncl, nch, mSD, tmp);
-				Fermeture5(nrl, nrh, ncl, nch, mSD, tmp);
-			
-			
-				sprintf(filename,"SD+morpho/hall%06d.pgm", step);
-				SavePGM_ui8matrix(mSD, nrl, nrh, ncl, nch, filename);*/
-
-				if(step%10 == 0)
-				{
-					Generate_ROC(mSD, nrl, nrh, ncl, nch, ROC_SD, step);
-					Generate_ROC(mFD, nrl, nrh, ncl, nch, ROC_FD, step);
-				}
-			}
-		}
+		test_FD(m, nrl, nrh, ncl, nch);
 		t2 = clock();
-		//test_visage();
-		t = (float)(t2 - t1) / CLOCKS_PER_SEC;
-		printf("end after %fs\n", t);
+		test_SD(m, nrl, nrh, ncl, nch);
+		t3 = clock();
+		test_morpho_SD(m, nrl, nrh, ncl, nch);
+		t4 = clock();
+		test_morpho_FD(m, nrl, nrh, ncl, nch);
+		t5 = clock();
+	
+		tfile = clock();
+		temps_fichier(m, nrl, nrh, ncl, nch);
+		tfile = clock() - tfile;
+
+		test_ROC(m, nrl, nrh, ncl, nch, ROC_FD, "FD");
+		test_ROC(m, nrl, nrh, ncl, nch, ROC_SD, "SD");
+		test_ROC(m, nrl, nrh, ncl, nch, ROC_FD_morpho, "FD+morpho");
+		test_ROC(m, nrl, nrh, ncl, nch, ROC_SD_morpho, "SD+morpho");
+		
+		//Affichage des temps de calculs
+		//Temps
+		temps_FD = (float)(t2 - t1 - tfile) / CLOCKS_PER_SEC;
+		temps_SD = (float)(t3 - t2 - tfile) / CLOCKS_PER_SEC;
+		temps_morpho_SD = (float)(t4 - t3 - tfile) / CLOCKS_PER_SEC;
+		temps_morpho_FD = (float)(t5 - t4 - tfile) / CLOCKS_PER_SEC;
+		//Cycles
+		cycle_FD = (t2 - t1 - tfile);
+		cycle_SD = (t3 - t2 - tfile);
+		cycle_morpho_SD = (t4 - t3 - tfile);
+		cycle_morpho_FD = (t5 - t4 - tfile);
+		//Affichage
+		printf("Pour FD:\nt = %.3f | cycle = %d\n",temps_FD, cycle_FD);
+		printf("Pour SD:\nt = %.3f | cycle = %d\n",temps_SD, cycle_SD);
+		printf("Pour les morphos sur SD:\nt = %.3f | cycle = %d\n",temps_morpho_SD, cycle_morpho_SD);
+		printf("Pour les morphos sur FD:\nt = %.3f | cycle = %d\n\n\n\n",temps_morpho_FD, cycle_morpho_FD);
+
+
+		//Affichage de la matrice ROC
 		printf("Pour SD :\nVP = %d, FN = %d, FP = %d, VN = %d\n",ROC_SD[0]/30,ROC_SD[1]/30,ROC_SD[2]/30,ROC_SD[3]/30);
 		float p = (float)(ROC_SD[0] + ROC_SD[3]) / (float)(ROC_SD[0] + ROC_SD[1] + ROC_SD[2] + ROC_SD[3]) * 100.0;
-		printf("%.2f poucent de pixels vraie\n\n", p);
+		printf("%.2f poucent de pixels vrais\n", p);
+		p = (float)(ROC_SD[0]) / (float)(ROC_SD[0] + ROC_SD[2]) * 100.0;
+		printf("%.2f poucent de pixels en mouvement vrais\n\n", p);
+
 		printf("Pour FD :\nVP = %d, FN = %d, FP = %d, VN = %d\n",ROC_FD[0]/30,ROC_FD[1]/30,ROC_FD[2]/30,ROC_FD[3]/30);
 		p = (float)(ROC_FD[0] + ROC_FD[3]) / (float)(ROC_FD[0] + ROC_FD[1] + ROC_FD[2] + ROC_FD[3]) * 100.0;
-		printf("%.2f poucent de pixels vraie\n", p);
+		printf("%.2f poucent de pixels vrais\n", p);
+		p = (float)(ROC_FD[0]) / (float)(ROC_FD[0] + ROC_FD[2]) * 100.0;
+		printf("%.2f poucent de pixels en mouvement vrais\n\n", p);
+
+		printf("Pour SD + morpho :\nVP = %d, FN = %d, FP = %d, VN = %d\n",ROC_SD_morpho[0]/30,ROC_SD_morpho[1]/30,ROC_SD_morpho[2]/30,ROC_SD_morpho[3]/30);
+		p = (float)(ROC_SD_morpho[0] + ROC_SD_morpho[3]) / (float)(ROC_SD_morpho[0] + ROC_SD_morpho[1] + ROC_SD_morpho[2] + ROC_SD_morpho[3]) * 100.0;
+		printf("%.2f poucent de pixels vrais\n", p);
+		p = (float)(ROC_SD_morpho[0]) / (float)(ROC_SD_morpho[0] + ROC_SD_morpho[2]) * 100.0;
+		printf("%.2f poucent de pixels en mouvement vrais\n\n", p);;
+
+		printf("Pour FD + morpho:\nVP = %d, FN = %d, FP = %d, VN = %d\n",ROC_FD_morpho[0]/30,ROC_FD_morpho[1]/30,ROC_FD_morpho[2]/30,ROC_FD_morpho[3]/30);
+		p = (float)(ROC_FD_morpho[0] + ROC_FD_morpho[3]) / (float)(ROC_FD_morpho[0] + ROC_FD_morpho[1] + ROC_FD_morpho[2] + ROC_FD_morpho[3]) * 100.0;
+		printf("%.2f poucent de pixels vrais\n", p);
+		p = (float)(ROC_FD_morpho[0]) / (float)(ROC_FD_morpho[0] + ROC_FD_morpho[2]) * 100.0;
+		printf("%.2f poucent de pixels en mouvement vrais\n", p);
 
 	}
 	else
